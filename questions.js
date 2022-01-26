30469bdc1e182331a203e112b85f69c7edb28ec3
puspalata123 <96433900+puspalata123@users.noreply.github.com>
2022-01-27 00:08:53 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions=[
+    {
+        numb: 1,
+        question: "JavaScript is the ____ type language.",
+        answer:"Object-Oriented",
+        options:[
+        "Object-Oriented",
+        "Object Based",
+        "Procedural",
+        "None of the above"
+        ]
+        
+    },
+    {
+        numb:2,
+        question: "What keyword is used to check whether a given property is valid or not?",
+        answer:"in",
+        options:[
+        "in",
+        "is in",
+        "exists",
+        "lies"
+        ]
+    },
+    {
+        numb:3,
+        question: "What is the use of the <nonscript> tag in JavaScript?",
+        answer:"The contents are displayed by non-JS-based browsers.",
+        options:[
+        "The contents are displayed by non-JS-based browsers.",
+        "Clears all the cookies and cache.",
+        "Both A and B.",
+        "None of the above"
+        ]
+    },
+    {
+        numb:4,
+        question:"Q4: When an operator's value is NULL, the type of returned by the unary operator is.",
+        answer:"Object",
+        options:[
+        "Boolean",
+        "Undefined",
+        "Object",
+        "Integer"
+        ]
+    },
+    {
+        numb:5,
+        question:"Q5: What does the JavaScript 'debugger' statements do?",
+        answer:"It acts as a breakpoint in a program",
+        options:[
+        "It will debug all the errors in the program at runtime.",
+        "It acts as a breakpoint in a program",
+        "It will debug error in the current statement if any",
+        "All of the above"
+        ]
+    },
+    {
+        numb:6,
+        question:"What does the 'toLocateString()' method do in JS?",
+        answer:"Returns a localised string representation of an object.",
+        options:[
+        "Returns a localised object representation.",
+        "Returns a parsed string.",
+        "Returns a localised string representation of an object.",
+        "None of the above."
+        ]
+    },
+    {
+        numb:7,
+        question:"The process in which an object or data structure is translated into a format suitable for transferral over a network, or storage is called?",
+        answer:"Object Serialization",
+        options:[
+        "Object Serialization",
+        "Object Encapsulation",
+        "Object Inheritance",
+        "None of the above",
+        ]
+    },
+    {
+        numb:8,
+        question:"Which function is used to serialize an object into a JSON string in Javascript?",
+        answer:"stringify()",
+        options:[
+        "stringify()",
+        "parse()",
+        "convert()",
+        "None of the above",
+        ]
+    },
+    {
+        numb:9,
+        question:"The 3 basic object attributes in Javascript are:",
+        answer:"class, prototype, Object's extensible flag.",
+        options:[
+        "Class, prototype, objects' parameters.",
+        "class, prototype, Object's extensible flag.",
+        "class, parameters, object's extensible flag.",
+        "Classes, Native object, and Interfaces and Object's extensible flag.",
+        ]
+    },
+    {
+
+        question:"Q10: Which of the following is not a Javascript framework?",
+        a:"Node",
+        b:"Vue",
+        c:"React",
+        d:"Cassandra",
+        ans:"ans4"
+    }
+
+];
